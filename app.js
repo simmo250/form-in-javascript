d0524872373f7d8ecbc9fb3bb7f0328e222aa60c
Simran gupta <71872452+simmo250@users.noreply.github.com>
2024-02-06 07:32:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function myFunction(){
+    let x = document.getElementById("numb").value;
+    let text;
+    if (isNaN(x) || x < 1 || x > 10) {
+        text = "Input not valid";
+      } else {
+        text = "Input OK";
+      }
+    document.getElementById("demo").innerHTML=text;
+}
